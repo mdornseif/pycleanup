1bf33ffa00e3c4373b7330df2593b5c7067ccf5c
guido.van.rossum <guido.van.rossum@6015fed2-1504-0410-9fe1-9d1591cc4771>
2006-12-12 15:25:19 +0000

    Complete the transformation -- all fixers now live in the 'fixes' package.
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@53010 6015fed2-1504-0410-9fe1-9d1591cc4771

--- fixes/fix_print.py
+++ fixes/fix_print.py
@@ -1,40 +1,100 @@
 # Copyright 2006 Google, Inc. All Rights Reserved.
 # Licensed to PSF under a Contributor Agreement.
 
-"""Null fixer.  Use as a template."""
+"""Fixer for print.
+
+Change:
+    'print'          into 'Print()'
+    'print ...'	     into 'Print(...)'
+    'print ... ,'    into 'Print(..., end=" ")'
+    'print >>x, ...' into 'Print(..., file=x)'
+"""
+
+# Python imports
+import token
+
+# Local imports
+import pytree
+import patcomp
+import pygram
+
+syms = pygram.python_symbols
+pat_compile = patcomp.PatternCompiler().compile_pattern
+
+PATTERN = """
+'print' | print_stmt
+"""
 
 
-class FixNull(object):
-
-    """Fixer class.
-
-    The class name must be FixFooBar where FooBar is the result of
-    removing underscores and capitalizing the words of the fix name.
-    For example, the class name for a fixer named 'has_key' should be
-    FixHasKey.
-    """
+class FixPrint(object):
 
     def __init__(self, options):
-        """Initializer.
-
-        The argument is an optparse.Values instance which can be used
-        to inspect the command line options.
-        """
         self.options = options
+        self.pattern = pat_compile(PATTERN)
 
     def match(self, node):
-        """Matcher.
-
-        Should return a true or false object (not necessarily a bool).
-        It may return a non-empty dict of matching sub-nodes as
-        returned by a matching pattern.
-        """
-        return None
+        if node.parent is not None and node.parent.type == syms.print_stmt:
+            # Avoid matching 'print' as part of a print_stmt
+            return None
+        return self.pattern.match(node)
 
     def transform(self, node):
-        """Transformer.
+        results = self.match(node)
+        assert results
 
-        Should return None, or a node that is a modified copy of the
-        argument node.  The argument should not be modified in place.
-        """
-        return None
+        if node == pytree.Leaf(token.NAME, "print"):
+            # Special-case print all by itself
+            new = pytree.Node(syms.power,
+                              (pytree.Leaf(token.NAME, "Print"),
+                               pytree.Node(syms.trailer,
+                                           (pytree.Leaf(token.LPAR, "("),
+                                            pytree.Leaf(token.RPAR, ")")))))
+            new.set_prefix(node.get_prefix())
+            return new
+        assert node.children[0] == pytree.Leaf(token.NAME, "print")
+        args = node.children[1:]
+        sep = end = file = None
+        if args and args[-1] == pytree.Leaf(token.COMMA, ","):
+            args = args[:-1]
+            end = " "
+        if args and args[0] == pytree.Leaf(token.RIGHTSHIFT, ">>"):
+            assert len(args) >= 2
+            file = args[1].clone()
+            args = args[3:] # Strip a possible comma after the file expression
+        # Now synthesize a Print(args, sep=..., end=..., file=...) node.
+        n_print = pytree.Leaf(token.NAME, "Print") # XXX -> "print"
+        l_args = [arg.clone() for arg in args]
+        if l_args:
+            l_args[0].set_prefix("")
+        if sep is not None or end is not None or file is not None:
+            if sep is not None:
+                add_kwarg(l_args, "sep",
+                          pytree.Leaf(token.STRING, repr(sep)))
+            if end is not None:
+                add_kwarg(l_args, "end",
+                          pytree.Leaf(token.STRING, repr(end)))
+            if file is not None:
+                add_kwarg(l_args, "file", file)
+        if l_args:
+            n_arglist = pytree.Node(syms.arglist, l_args)
+        else:
+            n_arglist = None
+        l_args = [pytree.Leaf(token.LPAR, "("), pytree.Leaf(token.RPAR, ")")]
+        if n_arglist:
+            l_args.insert(1, n_arglist)
+        n_trailer = pytree.Node(syms.trailer, l_args)
+        n_stmt = pytree.Node(syms.power, (n_print, n_trailer))
+        n_stmt.set_prefix(node.get_prefix())
+        return n_stmt
+
+def add_kwarg(l_nodes, s_kwd, n_expr):
+    # XXX All this prefix-setting may lose comments (though rarely)
+    n_expr.set_prefix("")
+    n_argument = pytree.Node(syms.argument,
+                             (pytree.Leaf(token.NAME, s_kwd),
+                              pytree.Leaf(token.EQUAL, "="),
+                              n_expr))
+    if l_nodes:
+        l_nodes.append(pytree.Leaf(token.COMMA, ","))
+        n_argument.set_prefix(" ")
+    l_nodes.append(n_argument)
