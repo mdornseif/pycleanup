c587cc4913e126e26ec8359991a60c13805009ec
collin.winter <collin.winter@6015fed2-1504-0410-9fe1-9d1591cc4771>
2007-03-21 03:14:02 +0000

    Take advantage of the new Number macro.
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@54475 6015fed2-1504-0410-9fe1-9d1591cc4771

--- fixes/fix_long.py
+++ fixes/fix_long.py
@@ -10,7 +10,7 @@ This also strips the trailing 'L' or 'l' from long loterals.
 import pytree
 from pgen2 import token
 from fixes import basefix
-from fixes.util import Name
+from fixes.util import Name, Number
 
 
 class FixLong(basefix.BaseFix):
@@ -31,7 +31,7 @@ class FixLong(basefix.BaseFix):
             assert node == self.static_long, node
             new = self.static_int.clone()
         if number and node.value[-1] in ("l", "L"):
-            new = pytree.Leaf(token.NUMBER, node.value[:-1])
+            new = Number(node.value[:-1])
         if new is not None:
             new.set_prefix(node.get_prefix())
             return new
