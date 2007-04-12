9691c8679a537b74502a8a1226ecc528884dd705
collin.winter <collin.winter@6015fed2-1504-0410-9fe1-9d1591cc4771>
2007-04-12 03:30:26 +0000

    Reindent, reformat for readability. Also, remove an unneeded import.
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@54770 6015fed2-1504-0410-9fe1-9d1591cc4771

--- fixes/fix_raw_input.py
+++ fixes/fix_raw_input.py
@@ -3,23 +3,19 @@
 
 # Local imports
 import pytree
-from pgen2 import token
 from fixes import basefix
 from fixes.util import Name
 
 class FixRawInput(basefix.BaseFix):
 
     PATTERN = """
-    power<
-        'raw_input'
-        args=trailer< '(' [any] ')' >
-    >
-    """
+              power< 'raw_input' args=trailer< '(' [any] ')' > >
+              """
 
     def transform(self, node):
         results = self.match(node)
         args = results["args"]
-        new = pytree.Node(self.syms.power,
-                          [Name("input"), args.clone()])
+
+        new = pytree.Node(self.syms.power, [Name("input"), args.clone()])
         new.set_prefix(node.get_prefix())
         return new
