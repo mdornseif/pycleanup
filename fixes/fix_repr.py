81c8c21d3b08f562e206a202a51014a2964af363
collin.winter <collin.winter@6015fed2-1504-0410-9fe1-9d1591cc4771>
2007-04-12 03:34:39 +0000

    Reindent, reformat for readability. Also, remove an unneeded import.
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@54771 6015fed2-1504-0410-9fe1-9d1591cc4771

--- fixes/fix_repr.py
+++ fixes/fix_repr.py
@@ -5,7 +5,6 @@
 
 # Local imports
 import pytree
-from pgen2 import token
 from fixes import basefix
 from fixes.util import Call, Name
 
@@ -13,15 +12,16 @@ from fixes.util import Call, Name
 class FixRepr(basefix.BaseFix):
 
     PATTERN = """
-    atom < '`' expr=any '`' >
-    """
+              atom < '`' expr=any '`' >
+              """
 
     def transform(self, node):
       results = self.match(node)
       assert results
       expr = results["expr"].clone()
+
       if expr.type == self.syms.testlist1:
-        expr = self.parenthesize(expr)
+          expr = self.parenthesize(expr)
       new = Call(Name("repr"), [expr])
       new.set_prefix(node.get_prefix())
       return new
