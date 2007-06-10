4ba67722fe9e6a80de6219aff9975169298aa5a3
georg.brandl <georg.brandl@6015fed2-1504-0410-9fe1-9d1591cc4771>
2007-06-10 18:31:01 +0000

    Remove the parts of the numliterals fixer that aren't PEP compliant.
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@55857 6015fed2-1504-0410-9fe1-9d1591cc4771

--- fixes/fix_numliterals.py
+++ fixes/fix_numliterals.py
@@ -1,5 +1,4 @@
-"""Fixer that turns 1L into 1, 0755 into 0o755,
-0XABC into 0xABC, 1E5 into 1e5, 1J into 1j.
+"""Fixer that turns 1L into 1, 0755 into 0o755.
 """
 # Copyright 2007 Georg Brandl.
 # Licensed to PSF under a Contributor Agreement.
@@ -16,21 +15,12 @@ class FixNumliterals(basefix.BaseFix):
     def match(self, node):
         # Override
         return (node.type == token.NUMBER and
-                (node.value.startswith("0") or
-                 'E' in node.value or
-                 'J' in node.value or
-                 node.value[-1] in "Ll"))
+                (node.value.startswith("0") or node.value[-1] in "Ll"))
 
     def transform(self, node):
         val = node.value
         if val[-1] in 'Ll':
             val = val[:-1]
-        if 'J' in val:
-            val = val.replace('J', 'j')
-        if 'E' in val:
-            val = val.replace('E', 'e')
-        if val.startswith('0X'):
-            val = '0x' + val[2:]
         elif val.startswith('0') and val.isdigit() and len(set(val)) > 1:
             val = "0o" + val[1:]
 
