4f652568ed6bea75ae1693888a323a907b687b64
collin.winter <collin.winter@6015fed2-1504-0410-9fe1-9d1591cc4771>
2007-06-25 18:00:27 +0000

    Incorporate example.py and the fixer modules as test corpora.
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@56087 6015fed2-1504-0410-9fe1-9d1591cc4771

--- tests/test_fixers.py
+++ tests/test_fixers.py
@@ -7,8 +7,10 @@ import support
 
 # Python imports
 from StringIO import StringIO
-import unittest
 import logging
+import os
+import os.path
+import unittest
 
 # Local imports
 import pytree
@@ -1800,6 +1802,27 @@ class Test_callable(FixerTestCase):
 
         a = """callable(x, kw=y)"""
         self.check(a, a)
+
+class Test_all(FixerTestCase):
+    fixer = "all"
+
+    def test_examples_file(self):
+        # Just test that we can parse examples.py without failing
+        basedir = os.path.dirname(refactor.__file__)
+        example = os.path.join(basedir, "example.py")
+        self.refactor_stream("example.py", open(example))
+
+    def test_fixers(self):
+        # Just test that we can parse all the fixers without failing
+        basedir = os.path.dirname(refactor.__file__)
+        fixerdir = os.path.join(basedir, "fixes")
+        for filename in os.listdir(fixerdir):
+            if not filename.endswith(".py"):
+                print "Skipping %s" % filename
+                continue
+            print "Testing fixer %s..." % filename
+            fixer = os.path.join(fixerdir, filename)
+            self.refactor_stream(fixer, open(fixer))
 
 
 if __name__ == "__main__":
