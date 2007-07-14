c25f3c45080b0733ffe71a31b78232dd3b72c4f6
collin.winter <collin.winter@6015fed2-1504-0410-9fe1-9d1591cc4771>
2007-07-14 22:34:48 +0000

    Add a fixer to convert 'for x in f.xreadlines():' into 'for x in f:'.
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@56390 6015fed2-1504-0410-9fe1-9d1591cc4771

--- tests/test_fixers.py
+++ tests/test_fixers.py
@@ -1200,6 +1200,49 @@ class Test_funcattrs(FixerTestCase):
             self.check(s, s)
 
 
+class Test_xreadlines(FixerTestCase):
+    fixer = "xreadlines"
+
+    def test_call(self):
+        b = "for x in f.xreadlines(): pass"
+        a = "for x in f: pass"
+        self.check(b, a)
+
+        b = "for x in foo().xreadlines(): pass"
+        a = "for x in foo(): pass"
+        self.check(b, a)
+
+        b = "for x in (5 + foo()).xreadlines(): pass"
+        a = "for x in (5 + foo()): pass"
+        self.check(b, a)
+
+    def test_attr_ref(self):
+        b = "foo(f.xreadlines + 5)"
+        a = "foo(f.__iter__ + 5)"
+        self.check(b, a)
+
+        b = "foo(f().xreadlines + 5)"
+        a = "foo(f().__iter__ + 5)"
+        self.check(b, a)
+
+        b = "foo((5 + f()).xreadlines + 5)"
+        a = "foo((5 + f()).__iter__ + 5)"
+        self.check(b, a)
+
+    def test_unchanged(self):
+        s = "for x in f.xreadlines(5): pass"
+        self.check(s, s)
+
+        s = "for x in f.xreadlines(k=5): pass"
+        self.check(s, s)
+
+        s = "for x in f.xreadlines(*k, **v): pass"
+        self.check(s, s)
+
+        s = "foo(xreadlines)"
+        self.check(s, s)
+
+
 class Test_stringio(FixerTestCase):
     fixer = "stringio"
 
