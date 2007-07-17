8c345288ea43227a3a6055bce0a12a0721b70ce3
collin.winter <collin.winter@6015fed2-1504-0410-9fe1-9d1591cc4771>
2007-07-17 21:12:35 +0000

    Whitespace, line length tweaks.
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@56431 6015fed2-1504-0410-9fe1-9d1591cc4771

--- tests/test_util.py
+++ tests/test_util.py
@@ -21,7 +21,7 @@ def parse(code, strip_levels=0):
         tree = tree.children[0]
     tree.parent = None
     return tree
-    
+
 class MacroTestCase(support.TestCase):
     def assertStr(self, node, string):
         if isinstance(node, (tuple, list)):
@@ -39,7 +39,7 @@ class Test_is_tuple(support.TestCase):
         self.failUnless(self.is_tuple("((a, (b, c)),)"))
         self.failUnless(self.is_tuple("(a,)"))
         self.failUnless(self.is_tuple("()"))
-    
+
     def test_invalid(self):
         self.failIf(self.is_tuple("(a)"))
         self.failIf(self.is_tuple("('foo') % (b, c)"))
@@ -55,7 +55,7 @@ class Test_is_list(support.TestCase):
         self.failUnless(self.is_list("[a, b]"))
         self.failUnless(self.is_list("[a, [b, c]]"))
         self.failUnless(self.is_list("[[a, [b, c]],]"))
-        
+
     def test_invalid(self):
         self.failIf(self.is_list("[]+[]"))
 
@@ -64,25 +64,25 @@ class Test_Attr(MacroTestCase):
     def test(self):
         from fixes.util import Attr, Name
         call = parse("foo()", strip_levels=2)
-    
+
         self.assertStr(Attr(Name("a"), Name("b")), "a.b")
         self.assertStr(Attr(call, Name("b")), "foo().b")
-        
+
     def test_returns(self):
         from fixes.util import Attr, Name
-        
+
         attr = Attr(Name("a"), Name("b"))
         self.assertEqual(type(attr), list)
 
-        
+
 class Test_Name(MacroTestCase):
     def test(self):
         from fixes.util import Name
-        
+
         self.assertStr(Name("a"), "a")
         self.assertStr(Name("foo.foo().bar"), "foo.foo().bar")
         self.assertStr(Name("a", prefix="b"), "ba")
-        
+
 
 class Test_find_binding(support.TestCase):
     def find_binding(self, name, string):
@@ -100,7 +100,7 @@ class Test_find_binding(support.TestCase):
         self.failUnless(self.find_binding("a", "(a,) = b"))
         self.failUnless(self.find_binding("a", "(a, b, c) = [b, c, d]"))
         self.failUnless(self.find_binding("a", "(c, (d, a), b) = foo()"))
-        self.failUnless(self.find_binding("a", "(a, b) = foo().foo.foo[6][foo]"))
+        self.failUnless(self.find_binding("a", "(a, b) = foo().foo[6][foo]"))
         self.failIf(self.find_binding("a", "(foo, b) = (b, a)"))
         self.failIf(self.find_binding("a", "(foo, (b, c)) = (a, b, c)"))
 
@@ -108,22 +108,22 @@ class Test_find_binding(support.TestCase):
         self.failUnless(self.find_binding("a", "[a] = b"))
         self.failUnless(self.find_binding("a", "[a, b, c] = [b, c, d]"))
         self.failUnless(self.find_binding("a", "[c, [d, a], b] = foo()"))
-        self.failUnless(self.find_binding("a", "[a, b] = foo().foo.foo[a][foo]"))
+        self.failUnless(self.find_binding("a", "[a, b] = foo().foo[a][foo]"))
         self.failIf(self.find_binding("a", "[foo, b] = (b, a)"))
         self.failIf(self.find_binding("a", "[foo, [b, c]] = (a, b, c)"))
-        
+
     def test_invalid_assignments(self):
         self.failIf(self.find_binding("a", "foo.a = 5"))
         self.failIf(self.find_binding("a", "foo[a] = 5"))
         self.failIf(self.find_binding("a", "foo(a) = 5"))
         self.failIf(self.find_binding("a", "foo(a, b) = 5"))
-        
+
     def test_simple_import(self):
         self.failUnless(self.find_binding("a", "import a"))
         self.failUnless(self.find_binding("a", "import b, c, a, d"))
         self.failIf(self.find_binding("a", "import b"))
         self.failIf(self.find_binding("a", "import b, c, d"))
-        
+
     def test_from_import(self):
         self.failUnless(self.find_binding("a", "from x import a"))
         self.failUnless(self.find_binding("a", "from a import a"))
@@ -133,13 +133,13 @@ class Test_find_binding(support.TestCase):
         self.failIf(self.find_binding("a", "from a import b"))
         self.failIf(self.find_binding("a", "from a.d import b"))
         self.failIf(self.find_binding("a", "from d.a import b"))
-        
+
     def test_import_as(self):
         self.failUnless(self.find_binding("a", "import b as a"))
         self.failUnless(self.find_binding("a", "import b as a, c, a as f, d"))
         self.failIf(self.find_binding("a", "import a as f"))
         self.failIf(self.find_binding("a", "import b, c as f, d as e"))
-        
+
     def test_from_import_as(self):
         self.failUnless(self.find_binding("a", "from x import b as a"))
         self.failUnless(self.find_binding("a", "from x import g as a, d as b"))
@@ -148,7 +148,7 @@ class Test_find_binding(support.TestCase):
         self.failIf(self.find_binding("a", "from a import b as t"))
         self.failIf(self.find_binding("a", "from a.d import b as t"))
         self.failIf(self.find_binding("a", "from d.a import b as t"))
-        
+
     def test_function_def(self):
         self.failUnless(self.find_binding("a", "def a(): pass"))
         self.failUnless(self.find_binding("a", "def a(b, c, d): pass"))
@@ -157,13 +157,13 @@ class Test_find_binding(support.TestCase):
         self.failIf(self.find_binding("a", "def d(a=7): pass"))
         self.failIf(self.find_binding("a", "def d(a): pass"))
         self.failIf(self.find_binding("a", "def d(): a = 7"))
-        
+
         s = """
             def d():
                 def a():
                     pass"""
         self.failIf(self.find_binding("a", s))
-        
+
     def test_class_def(self):
         self.failUnless(self.find_binding("a", "class a: pass"))
         self.failUnless(self.find_binding("a", "class a(): pass"))
@@ -175,13 +175,13 @@ class Test_find_binding(support.TestCase):
         self.failIf(self.find_binding("a", "class d(b, *a): pass"))
         self.failIf(self.find_binding("a", "class d(b, **a): pass"))
         self.failIf(self.find_binding("a", "class d: a = 7"))
-        
+
         s = """
             class d():
                 class a():
                     pass"""
         self.failIf(self.find_binding("a", s))
-        
+
     def test_for(self):
         self.failUnless(self.find_binding("a", "for a in r: pass"))
         self.failUnless(self.find_binding("a", "for a, b in r: pass"))
@@ -190,90 +190,90 @@ class Test_find_binding(support.TestCase):
         self.failUnless(self.find_binding("a", "for c, (a, b) in r: pass"))
         self.failUnless(self.find_binding("a", "for c in r: a = c"))
         self.failIf(self.find_binding("a", "for c in a: pass"))
-        
+
     def test_for_nested(self):
         s = """
             for b in r:
                 for a in b:
                     pass"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             for b in r:
                 for a, c in b:
                     pass"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             for b in r:
                 for (a, c) in b:
                     pass"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             for b in r:
                 for (a,) in b:
                     pass"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             for b in r:
                 for c, (a, d) in b:
                     pass"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             for b in r:
                 for c in b:
                     a = 7"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             for b in r:
                 for c in b:
                     d = a"""
         self.failIf(self.find_binding("a", s))
-        
+
         s = """
             for b in r:
                 for c in a:
                     d = 7"""
         self.failIf(self.find_binding("a", s))
-        
+
     def test_if(self):
         self.failUnless(self.find_binding("a", "if b in r: a = c"))
         self.failIf(self.find_binding("a", "if a in r: d = e"))
-    
+
     def test_if_nested(self):
         s = """
             if b in r:
                 if c in d:
                     a = c"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             if b in r:
                 if c in d:
                     c = a"""
         self.failIf(self.find_binding("a", s))
-        
+
     def test_while(self):
         self.failUnless(self.find_binding("a", "while b in r: a = c"))
         self.failIf(self.find_binding("a", "while a in r: d = e"))
-    
+
     def test_while_nested(self):
         s = """
             while b in r:
                 while c in d:
                     a = c"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             while b in r:
                 while c in d:
                     c = a"""
         self.failIf(self.find_binding("a", s))
-        
+
     def test_try_except(self):
         s = """
             try:
@@ -281,14 +281,14 @@ class Test_find_binding(support.TestCase):
             except:
                 b = 8"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
             except:
                 a = 6"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
@@ -297,14 +297,14 @@ class Test_find_binding(support.TestCase):
             except:
                 a = 6"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
             except:
                 b = 6"""
         self.failIf(self.find_binding("a", s))
-        
+
     def test_try_except_nested(self):
         s = """
             try:
@@ -315,7 +315,7 @@ class Test_find_binding(support.TestCase):
             except:
                 b = 8"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
@@ -325,7 +325,7 @@ class Test_find_binding(support.TestCase):
                 except:
                     pass"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
@@ -335,7 +335,7 @@ class Test_find_binding(support.TestCase):
                 except:
                     a = 6"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 try:
@@ -347,7 +347,7 @@ class Test_find_binding(support.TestCase):
             except:
                 pass"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 pass
@@ -359,14 +359,14 @@ class Test_find_binding(support.TestCase):
                 except:
                     a = 6"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
             except:
                 b = 6"""
         self.failIf(self.find_binding("a", s))
-        
+
         s = """
             try:
                 try:
@@ -381,7 +381,7 @@ class Test_find_binding(support.TestCase):
                 except:
                     o = y"""
         self.failIf(self.find_binding("a", s))
-        
+
     def test_try_except_finally(self):
         s = """
             try:
@@ -391,21 +391,21 @@ class Test_find_binding(support.TestCase):
             finally:
                 a = 9"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
             finally:
                 a = 6"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
             finally:
                 b = 6"""
         self.failIf(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
@@ -414,7 +414,7 @@ class Test_find_binding(support.TestCase):
             finally:
                 b = 6"""
         self.failIf(self.find_binding("a", s))
-        
+
     def test_try_except_finally_nested(self):
         s = """
             try:
@@ -429,7 +429,7 @@ class Test_find_binding(support.TestCase):
                 finally:
                     c = 9"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
@@ -439,7 +439,7 @@ class Test_find_binding(support.TestCase):
                 finally:
                     a = 6"""
         self.failUnless(self.find_binding("a", s))
-        
+
         s = """
             try:
                 b = 8
@@ -449,7 +449,7 @@ class Test_find_binding(support.TestCase):
                 finally:
                     b = 7"""
         self.failIf(self.find_binding("a", s))
-        
+
 
 if __name__ == "__main__":
     import __main__
