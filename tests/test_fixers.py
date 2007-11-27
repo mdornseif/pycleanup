2d3b226a1c3ec4a5a08aeaf9583f24b54f58fc80
christian.heimes <christian.heimes@6015fed2-1504-0410-9fe1-9d1591cc4771>
2007-11-27 19:14:10 +0000

    Added fixer for method attributes based on fix_funcattrs.py
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@59196 6015fed2-1504-0410-9fe1-9d1591cc4771

--- tests/test_fixers.py
+++ tests/test_fixers.py
@@ -1576,6 +1576,39 @@ class Test_tuple_params(FixerTestCase):
         a = """lambda x_y_z: x_y_z[0] + x_y_z[1][0] + f(x_y_z[1][0])"""
         self.check(b, a)
 
+class Test_methodattrs(FixerTestCase):
+    fixer = "methodattrs"
+
+    attrs = ["func", "self", "class"]
+
+    def test(self):
+        for attr in self.attrs:
+            b = "a.im_%s" % attr
+            if attr == "class":
+                a = "a.__self__.__class__"
+            else:
+                a = "a.__%s__" % attr
+            self.check(b, a)
+
+            b = "self.foo.im_%s.foo_bar" % attr
+            if attr == "class":
+                a = "self.foo.__self__.__class__.foo_bar"
+            else:
+                a = "self.foo.__%s__.foo_bar" % attr
+            self.check(b, a)
+
+    def test_unchanged(self):
+        for attr in self.attrs:
+            s = "foo(im_%s + 5)" % attr
+            self.unchanged(s)
+
+            s = "f(foo.__%s__)" % attr
+            self.unchanged(s)
+
+            s = "f(foo.__%s__.foo)" % attr
+            self.unchanged(s)
+
+
 class Test_next(FixerTestCase):
     fixer = "next"
 
