1c154437f8b3b16f499ee1352fb8c1a2d1f2e441
david.wolever <david.wolever@6015fed2-1504-0410-9fe1-9d1591cc4771>
2008-03-17 22:45:13 +0000

    Added code and tests to fix itertools in 2to3 (#2360, #2361, #2362, #2364).
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@61466 6015fed2-1504-0410-9fe1-9d1591cc4771

--- lib2to3/tests/test_fixers.py
+++ lib2to3/tests/test_fixers.py
@@ -265,6 +265,15 @@ class Test_apply(FixerTestCase):
         s = """apply(f, args, kwds=kwds)"""
         self.unchanged(s)
 
+    def test_space_1(self):
+        a = """apply(  f,  args,   kwds)"""
+        b = """f(*args, **kwds)"""
+        self.check(a, b)
+
+    def test_space_2(self):
+        a = """apply(  f  ,args,kwds   )"""
+        b = """f(*args, **kwds)"""
+        self.check(a, b)
 
 class Test_intern(FixerTestCase):
     fixer = "intern"
@@ -2827,6 +2836,52 @@ class Test_future(FixerTestCase):
         b = """from __future__ import braces"""
         a = """"""
         self.check(b, a)
+
+class Test_itertools(FixerTestCase):
+    fixer = "itertools"
+
+    def checkall(self, before, after):
+        # Because we need to check with and without the itertools prefix
+        # and on each of the three functions, these loops make it all
+        # much easier
+        for i in ('itertools.', ''):
+            for f in ('map', 'filter', 'zip'):
+                b = before %(i+'i'+f)
+                a = after %(f)
+                self.check(b, a)
+
+    def test_0(self):
+        # A simple example -- test_1 covers exactly the same thing,
+        # but it's not quite as clear.
+        b = "itertools.izip(a, b)"
+        a = "zip(a, b)"
+        self.check(b, a)
+
+    def test_1(self):
+        b = """%s(f, a)"""
+        a = """%s(f, a)"""
+        self.checkall(b, a)
+
+    def test_2(self):
+        b = """itertools.ifilterfalse(a, b)"""
+        a = """itertools.filterfalse(a, b)"""
+        self.check(b, a)
+
+    def test_4(self):
+        b = """ifilterfalse(a, b)"""
+        a = """filterfalse(a, b)"""
+        self.check(b, a)
+
+    def test_space_1(self):
+        b = """    %s(f, a)"""
+        a = """    %s(f, a)"""
+        self.checkall(b, a)
+
+    def test_space_2(self):
+        b = """    itertools.ifilterfalse(a, b)"""
+        a = """    itertools.filterfalse(a, b)"""
+        self.check(b, a)
+
 
 if __name__ == "__main__":
     import __main__
