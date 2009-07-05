75c1e27d3e4cbd9c07916477df09465fff62186e
benjamin.peterson <benjamin.peterson@6015fed2-1504-0410-9fe1-9d1591cc4771>
2009-07-05 14:15:53 +0000

    cleanup and use unicode where appropiate
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@73861 6015fed2-1504-0410-9fe1-9d1591cc4771

--- lib2to3/fixes/fix_operator.py
+++ lib2to3/fixes/fix_operator.py
@@ -14,28 +14,27 @@ class FixOperator(fixer_base.BaseFix):
     func = "'(' func=any ')'"
     PATTERN = """
               power< module='operator'
-                trailer< '.' %(methods)s > trailer< %(func)s > >
+                trailer< '.' {methods} > trailer< {func} > >
               |
-              power< %(methods)s trailer< %(func)s > >
-              """ % locals()
+              power< {methods} trailer< {func} > >
+              """.format(methods=methods, func=func)
 
     def transform(self, node, results):
         method = results["method"][0]
 
-        if method.value == "sequenceIncludes":
+        if method.value == u"sequenceIncludes":
             if "module" not in results:
                 # operator may not be in scope, so we can't make a change.
                 self.warning(node, "You should use operator.contains here.")
             else:
-                method.value = "contains"
+                method.value = u"contains"
                 method.changed()
-                return node
-        elif method.value == "isCallable":
+        elif method.value == u"isCallable":
             if "module" not in results:
                 self.warning(node,
                              "You should use hasattr(%s, '__call__') here." %
                              results["func"].value)
             else:
                 func = results["func"]
-                args = [func.clone(), String(", "), String("'__call__'")]
-                return Call(Name("hasattr"), args, prefix=node.get_prefix())
+                args = [func.clone(), String(u", "), String(u"'__call__'")]
+                return Call(Name(u"hasattr"), args, prefix=node.get_prefix())
