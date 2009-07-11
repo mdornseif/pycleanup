dfdadbd1448f8b7aa96d7e4ac087a6345239bda5
benjamin.peterson <benjamin.peterson@6015fed2-1504-0410-9fe1-9d1591cc4771>
2009-07-11 22:33:35 +0000

    revert unintended change in 73965
    git-svn-id: http://svn.python.org/projects/sandbox/trunk/2to3@73966 6015fed2-1504-0410-9fe1-9d1591cc4771

--- lib2to3/refactor.py
+++ lib2to3/refactor.py
@@ -343,7 +343,8 @@ class RefactoringTool(object):
                 results = fixer.match(node)
                 if results:
                     new = fixer.transform(node, results)
-                    if new is not None:
+                    if new is not None and (new != node or
+                                            unicode(new) != unicode(node)):
                         node.replace(new)
                         node = new
 
